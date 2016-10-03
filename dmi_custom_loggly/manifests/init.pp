4a68263ad3c5324f15f9a9a3c699f0a34a9197d7
Sagar <sagar@Sagars-MacBook-Air.local>
2016-10-03 15:54:39 +0530

    Modified loggly module

--- dmi_custom_loggly/manifests/init.pp
+++ dmi_custom_loggly/manifests/init.pp
@@ -1,10 +1,13 @@
 class dmi_sd_loggly(
                   $customer = $customer,
+                  $mainconfigs = ["haproxy.conf", "emitter.conf", "nginx-clb.conf", "pending.conf", "test.conf", "dls.conf"],
 )
 {
+
   file {'/home/ec2-user/cloudmi/loggly':
     ensure => directory,
   }
+
   file {'/home/ec2-user/cloudmi/loggly/modules':
     ensure     => directory,
     #force      => true,
@@ -20,77 +23,16 @@ class dmi_sd_loggly(
 		group       => 'ec2-user',
     require     => File['/home/ec2-user/cloudmi/loggly/modules'],
 	}
-
-	file { '/home/ec2-user/cloudmi/loggly/modules/haproxy.conf':
-		content     => template('dmi_sd_loggly/haproxy.conf.erb'),
-		mode        => '0644',
-		owner       => 'ec2-user',
-		group       => 'ec2-user',
-    require     => File['/home/ec2-user/cloudmi/loggly/modules'],
-	}
-
-	file { '/home/ec2-user/cloudmi/loggly/modules/emitter.conf':
-		content     => template('dmi_sd_loggly/emitter.conf.erb'),
-		mode        => '0644',
-		owner       => 'ec2-user',
-		group       => 'ec2-user',
-    require     => File['/home/ec2-user/cloudmi/loggly/modules'],
-	}
-
-	file { '/home/ec2-user/cloudmi/loggly/modules/nginx-clb.conf':
-		content     => template('dmi_sd_loggly/nginx-clb.conf.erb'),
-		mode        => '0644',
-		owner       => 'ec2-user',
-		group       => 'ec2-user',
-    require     => File['/home/ec2-user/cloudmi/loggly/modules'],
-	}
-
-	file { '/home/ec2-user/cloudmi/loggly/modules/pending.conf':
-		content     => template('dmi_sd_loggly/pending.conf.erb'),
-		mode        => '0644',
-		owner       => 'ec2-user',
-		group       => 'ec2-user',
-    require     => File['/home/ec2-user/cloudmi/loggly/modules'],
-	}
-
-	file { '/home/ec2-user/cloudmi/loggly/modules/test.conf':
-		content     => template('dmi_sd_loggly/test.conf.erb'),
-		mode        => '0644',
-		owner       => 'ec2-user',
-		group       => 'ec2-user',
-    require     => File['/home/ec2-user/cloudmi/loggly/modules'],
-	}
-
-	file { '/home/ec2-user/cloudmi/loggly/modules/dls.conf':
-		content     => template('dmi_sd_loggly/dls.conf.erb'),
-		mode        => '0644',
-		owner       => 'ec2-user',
-		group       => 'ec2-user',
-    require     => File['/home/ec2-user/cloudmi/loggly/modules'],
-	}
-
-  class { 'supervisord':
-                        install_pip => true,
-                        log_path => "/logs/supervisord",
-                        log_file => "supervisord.log",
-                        logfile_maxbytes => 20,
-                        logfile_backups => 10,
-                        loglevel => info,
-        }
-  file { 'supervisord_symlink':
-    path          => '/usr/local/bin/supervisord',
-    ensure        => 'link',
-    #  force         => true,
-    target        => '/usr/bin/supervisord',
+  $mainconfigs.each |String $configfile| {
+  file { "/home/ec2-user/cloudmi/loggly/modules/${configfile}":
+    content => template("dmi_sd_loggly/${configfile}.erb"),
+    owner   => ec2-user,
+    group   => ec2-user,
+    mode    => '0644',
+    require => File['/home/ec2-user/cloudmi/loggly/modules'],
   }
-  file { 'supervisorctl_symlink':
-    path          => '/usr/local/bin/supervisorctl',
-    ensure        => 'link',
-    #  force         => true,
-    target        => '/usr/bin/supervisorctl',
-  }
-
-  supervisord::program { 'loggly':
+ }
+supervisord::program { 'loggly':
     command         => '/opt/td-agent/embedded/bin/ruby /usr/sbin/td-agent -c /home/ec2-user/cloudmi/loggly/td-agent.conf --group ec2-user --log /logs/td-agent/td-agent-loggly.log',
     directory       => '/home/ec2-user',
     autostart       => true,
